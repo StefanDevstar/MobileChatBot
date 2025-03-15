b179eff74ab87b45bced683b5de3fc68dc4e7f86
unknown <stefanescu.devstar@gmail.com>
2025-03-15 16:16:39 +0200

    goal image loader component

--- src/components/ImageLoader.js
+++ src/components/ImageLoader.js
@@ -3,7 +3,16 @@ import { useTheme } from '@react-navigation/native';
 import PropTypes from 'prop-types';
 import { View, ActivityIndicator } from 'react-native';
 
+const ImageLoader = ({ style }) => {
+  const theme = useTheme();
+  const { colors } = theme;
 
+  return (
+    <View style={style}>
+      <ActivityIndicator size="small" color={colors.textDark} />
+    </View>
+  );
+};
 
 const propTypes = {
   style: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.shape({})), PropTypes.shape({})]),
